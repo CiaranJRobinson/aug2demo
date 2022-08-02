9dadb6e0dea74a16cdb7b0536c7a7a6e5f5e189b
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-08-02 13:30:37 -0400

    Starting classes

--- demo.js
+++ demo.js
@@ -32,3 +32,36 @@ greeting();
 //destructure lots of things
 let {firstname, lastname, age} = person;
 console.log(firstname, lastname, age);
+
+//Rename age with destructuring to yearsOfWisdom.
+let {age: yearsOfWisdom} = person;
+console.log(yearsOfWisdom);
+
+//Review of looping objects using for...in loops
+for(const property in person){
+    console.log(`The property of ${property} has a value of: ${person[property]}`);
+}
+
+// Talking about adding and deleting properties
+//To add:
+person.job = "Instructor";
+console.log(person.job);
+
+//To remove:
+delete person.job;
+
+
+//CLasses!>?!?!
+
+class Dog {
+    constructor(name, breed, age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    greeting(){
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old. My breed is ${this.breed}.`);
+    }
+}
+
